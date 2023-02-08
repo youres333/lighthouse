f3669565724a65eaad0360a744199513c24f8734
Adam Raine <ad.st.raine@gmail.com>
2023-02-08 11:55:26 -0800

    start

---
+++ docs/recipes/custom-audit/memory-audit.js
@@ -0,0 +1,44 @@
+/**
+ * @license Copyright 2017 The Lighthouse Authors. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+ */
+
+import {Audit} from 'lighthouse';
+
+const MAX_MEMORY_USAGE = 1000;
+
+/**
+ * @fileoverview Tests that animation lengths are below the
+ * test threshold value.
+ */
+
+class MemoryUsage extends Audit {
+  static get meta() {
+    return {
+      id: 'memory-audit',
+      title: 'Did not find any large memory usage',
+      failureTitle: 'Found large memory usage',
+      description: 'Used to find any memory samples larger than 1kb',
+
+      // The name of the custom gatherer class that provides input to this audit.
+      requiredArtifacts: ['MemoryProfile'],
+    };
+  }
+
+  static audit(artifacts) {
+    let largestMemoryUsage = 0;
+    for (const sample of artifacts.MemoryProfile.samples) {
+      if (sample.total > largestMemoryUsage) {
+        largestMemoryUsage = sample.total;
+      }
+    }
+
+    return {
+      numericValue: largestMemoryUsage,
+      score: largestMemoryUsage > MAX_MEMORY_USAGE ? 0 : 1,
+    };
+  }
+}
+
+export default MemoryUsage;
