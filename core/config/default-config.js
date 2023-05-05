9d9da45b1a7bbe071be1825d4cfc9ffab0ad541b
Connor Clark <cjamcl@google.com>
2023-05-05 09:39:35 -0700

    new_audit: bring back redirects-http with passive https check

--- core/config/default-config.js
+++ core/config/default-config.js
@@ -176,6 +176,7 @@ const defaultConfig = {
   ],
   audits: [
     'is-on-https',
+    'redirects-http',
     'service-worker',
     'viewport',
     'metrics/first-contentful-paint',
@@ -560,6 +561,7 @@ const defaultConfig = {
       auditRefs: [
         // Trust & Safety
         {id: 'is-on-https', weight: 1, group: 'best-practices-trust-safety'},
+        {id: 'redirects-http', weight: 1, group: 'best-practices-trust-safety'},
         {id: 'geolocation-on-start', weight: 1, group: 'best-practices-trust-safety'},
         {id: 'notification-on-start', weight: 1, group: 'best-practices-trust-safety'},
         {id: 'csp-xss', weight: 0, group: 'best-practices-trust-safety'},
@@ -614,6 +616,7 @@ const defaultConfig = {
         {id: 'installable-manifest', weight: 2, group: 'pwa-installable'},
         // PWA Optimized
         {id: 'service-worker', weight: 1, group: 'pwa-optimized'},
+        {id: 'redirects-http', weight: 2, group: 'pwa-optimized'},
         {id: 'splash-screen', weight: 1, group: 'pwa-optimized'},
         {id: 'themed-omnibox', weight: 1, group: 'pwa-optimized'},
         {id: 'content-width', weight: 1, group: 'pwa-optimized'},
