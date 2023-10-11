15d8f0e0b7fb3735e9737a60cd82da992b8ba221
Adriana Ixba <ixbaadriana@gmail.com>
2023-10-11 12:17:09 -0700

    some tests fix

--- viewer/test/viewer-test-pptr.js
+++ viewer/test/viewer-test-pptr.js
@@ -395,7 +395,6 @@ describe('Lighthouse Viewer', () => {
           'accessibility',
           'seo',
           'best-practices',
-          'pwa',
         ],
         strategy: 'mobile',
         // These values aren't set by default.
@@ -425,7 +424,7 @@ describe('Lighthouse Viewer', () => {
     it('should call out to PSI with specified categories', async () => {
       psiResponse = goodPsiResponse;
 
-      const url = `${viewerUrl}?psiurl=https://www.example.com&category=seo&category=pwa&utm_source=utm&locale=es`;
+      const url = `${viewerUrl}?psiurl=https://www.example.com&category=seo&category=accessibility&utm_source=utm&locale=es`;
       await viewerPage.goto(url);
 
       // Wait for report to render.call out to PSI with specified categories
@@ -445,7 +444,7 @@ describe('Lighthouse Viewer', () => {
         url: 'https://www.example.com',
         category: [
           'seo',
-          'pwa',
+          'accessibility',
         ],
         locale: 'es',
         utm_source: 'utm',
