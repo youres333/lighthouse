dfad4c94c229c50b3265fe09ce14593872f42edf
Adam Raine <ad.st.raine@gmail.com>
2023-11-28 11:32:48 -0500

    rephrase

--- core/audits/third-party-cookies.js
+++ core/audits/third-party-cookies.js
@@ -18,7 +18,7 @@ const UIStrings = {
   /** Title of a Lighthouse audit that provides detail on the use of third party cookies. This descriptive title is shown to users when the page uses third party cookies. */
   failureTitle: 'Uses third-party cookies',
   /** Description of a Lighthouse audit that tells the user why they should not use third party cookies on their page. This is displayed after a user expands the section to see more. No character length limits. The last sentence starting with 'Learn' becomes link text to additional documentation. */
-  description: 'Third-party cookie support will be removed in a future version of Chrome. [Learn more about third-party cookie deprecation](https://developer.chrome.com/en/docs/privacy-sandbox/third-party-cookie-phase-out/).',
+  description: 'Support for third-party cookies will be removed in a future version of Chrome. [Learn more about phasing out third-party cookies](https://developer.chrome.com/en/docs/privacy-sandbox/third-party-cookie-phase-out/).',
   /** [ICU Syntax] Label for the audit identifying the number of third-party cookies. */
   displayValue: `{itemCount, plural,
     =1 {1 cookie found}
